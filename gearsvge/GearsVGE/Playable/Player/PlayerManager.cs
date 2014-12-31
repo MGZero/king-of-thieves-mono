542ca4903f7d4e649a84b8d57f9c002be122b60f
MGZero <onisephiroth@gmail.com>
2014-12-31 16:03:05 -0500

    blah
    Signed-off-by: MGZero <onisephiroth@gmail.com>

--- gearsvge/GearsVGE/Playable/Player/PlayerManager.cs
+++ gearsvge/GearsVGE/Playable/Player/PlayerManager.cs
@@ -5,11 +5,11 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
+
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
+
 using Microsoft.Xna.Framework.Storage;
 
 namespace Gears.Playable
